e49ce303fdb89b264a812e2c842bd3920a77615d
LuciusZhang <z360394624@gmail.com>
2017-05-02 23:03:12 +0800

    commit test

--- app/src/main/java/com/tec/lucius/MainActivity.java
+++ app/src/main/java/com/tec/lucius/MainActivity.java
@@ -3,11 +3,33 @@ package com.tec.lucius;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.tec.lucius.request.RetrofitRequest;
+import com.tec.lucius.response.ResponseSubscriber;
+
+import java.util.HashMap;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new SimpleRequest().method(RetrofitRequest.POST).execute("", new HashMap<String, Object>(), new ResponseSubscriber(){
+            @Override
+            public void onNext(Object o) {
+                super.onNext(o);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                super.onError(t);
+            }
+
+            @Override
+            public void onComplete() {
+                super.onComplete();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/tec/lucius/SimpleRequest.java
@@ -0,0 +1,12 @@
+package com.tec.lucius;
+
+import com.tec.lucius.request.BaseRequest;
+
+/**
+ * Created by z3603 on 2017/5/2.
+ */
+
+public class SimpleRequest extends BaseRequest {
+
+
+}
